204a9618a2ab8f83ca6a8f8b0f6d55f83b8623b0
Alexander0908 <sanya090895@gmail.com>
2024-06-26 23:32:21 +0300

    Updated AppHeader
    Updated AppHeader

--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,19 @@
+import { Link } from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
     return (
-        <header className="app__header">
+        <header className="app__header">    
             <h1 className="app__title">
-                <a href="#">
+                <Link to="/">
                     <span>Marvel</span> information portal
-                </a>
+                </Link>
             </h1>
             <nav className="app__menu">
                 <ul>
-                    <li><a href="#">Characters</a></li>
+                    <li><Link to="/">Characters</Link></li>
                     /
-                    <li><a href="#">Comics</a></li>
+                    <li><Link to="/comics">Comics</Link></li>
                 </ul>
             </nav>
         </header>
