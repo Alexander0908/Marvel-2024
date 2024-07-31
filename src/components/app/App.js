90ed74dba11556b458bfcfdd475514a5f3f69168
Alexander0908 <sanya090895@gmail.com>
2024-08-01 00:10:48 +0300

    Updates in App, SingleCharacter
    UpdatedUpdates in App, SingleCharacterLayout, SingleComicLayout

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,37 +1,43 @@
-import { lazy, Suspense } from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import AppHeader from "../appHeader/AppHeader";
-import Spinner from '../spinner/Spinner';
-// динамические импорты всегда лучше вставлять после статических
+import Spinner from "../spinner/Spinner";
+
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 const SinglePage = lazy(() => import('../pages/SinglePage'));
-// const SingleComicLayout = lazy(() => import('../pages/SingleComicLayout/SingleComicLayout'));
-// const SingleCharacterLayout = lazy(() => import('../pages/SingleCharacterLayout/SingleCharacterLayout'));
+// const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
 
 const App = () => {
 
     return (
-        <Router>
+       <>
+        <BrowserRouter>
             <div className="app">
-                <AppHeader/>
+                <AppHeader />
                 <main>
-                    <Suspense fallback={<Spinner/>}>
+                    <Suspense fallback={<Spinner />}>
                         <Routes>
-                            <Route path="/" element={<MainPage/>}/>
-                            <Route path="/comics" element={<ComicsPage/>}/>
-                            <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
-                            {/* <Route path="/comics/:comicId" element={<SingleComicLayout/>}/>
-                            <Route path="/comics/:comicId" element={<SingleCharacterLayout/>}/> */}
-                            <Route path="*" element={<Page404/>}/>
+                            <Route path='/' element={<MainPage />} />
+                            <Route path='/comics' element={<ComicsPage />} />
+                            {/* <Route path='/comics/:id'
+                                element={<SinglePage
+                                    Component={SingleComicLayout}
+                                    dataType='comic' />} /> */}
+                            <Route path='/characters/:id'
+                                element={<SinglePage
+                                    Component={SingleCharacterLayout}
+                                    dataType='character' />} />
+                            <Route path="*" element={<Page404 />} />
                         </Routes>
                     </Suspense>
                 </main>
             </div>
-        </Router>
+        </BrowserRouter>
+       </>
     )
 }
 
--- src/components/pages/SingleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/SingleCharacterLayout/SingleCharacterLayout.js
@@ -1,16 +1,22 @@
+import { Link } from 'react-router-dom';
+import './singleCharacterLayout.scss';
 
-import './singleComicPage.scss';
-
-const SingleCharacterLayout = ({data}) => {
-    const {title, description, thumbnail} = data;
+const SingleCharacterLayout = ({ data }) => {
+    const {name, description, thumbnail, title} = data;
 
     return (
         <div className="single-comic">
-            <img src={thumbnail} alt={title} className="single-comic__img"/>
+                <meta
+                    name="description"
+                    content={`${title} character`}
+                />
+                <title>{title}</title>
+            <img src={thumbnail} alt={name} className="single-comic__char-img"/>
             <div className="single-comic__info">
-                <h2 className="single-comic__name">{title}</h2>
+                <h2 className="single-comic__name">{name}</h2>
                 <p className="single-comic__descr">{description}</p>
             </div>
+            <Link to="/" className="single-comic__back">Back to all</Link>
         </div>
     )
 }
