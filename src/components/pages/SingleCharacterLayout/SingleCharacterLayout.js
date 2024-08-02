d9781ed9fd0729dfd9b4c080a67ae76a15c6fca9
Alexander0908 <sanya090895@gmail.com>
2024-08-02 20:13:31 +0300

    Update SingleCharacterLayout.js

--- src/components/pages/SingleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/SingleCharacterLayout/SingleCharacterLayout.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({ data }) => {
@@ -6,11 +8,13 @@ const SingleCharacterLayout = ({ data }) => {
 
     return (
         <div className="single-comic">
+            <Helmet>
                 <meta
                     name="description"
-                    content={`${title} character`}
+                    content={`${name} page`}
                 />
                 <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={name} className="single-comic__char-img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{name}</h2>
